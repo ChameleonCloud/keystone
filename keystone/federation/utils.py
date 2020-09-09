c80a51492d302203ebda9cc43c8040b7ad4eef52
Jason Anderson <jasonanderson@uchicago.edu>
2020-09-08 20:57:29 -0500

    Map keys on multi-valued direct-maps as list
    This makes a lot of interesting things possible, namely expressing
    direct-maps as "{0[key]}", meaning "For each value of the 0th remote
    value, get the item at key 'key'".
    
    Change-Id: I15d7a871477af9594b68241804b91ee8e7490fd1

--- keystone/federation/utils.py
+++ keystone/federation/utils.py
@@ -15,6 +15,7 @@
 import ast
 import copy
 import json
+import operator
 import re
 
 import flask
@@ -292,6 +293,34 @@ def get_default_attribute_mapping_schema_version():
     return CONF.federation.attribute_mapping_default_schema_version
 
 
+class DirectMap(object):
+    """An abstraction around an individual remote match.
+
+    When formatted, any item retrievals on the format syntax are treated as
+    a map operation, returning a list of values at that key for each item in
+    the match. For example, given:
+
+      d = DirectMap([{'name': 'foo'}, {'name': 'bar'}])
+
+    Normally "{name}".format(d) would fail to format, as no list index was
+    specified (would have to be "{0[name]}" or "{1[name]}"); instead, we get
+    the output "['foo', 'bar']".
+    """
+    def __init__(self, entry):
+        self._entry = entry
+
+    def __str__(self):
+        """return the direct map entry as a string."""
+        return '%s' % self._entry
+
+    def __getitem__(self, key):
+        """Used by Python when executing ``''.format(*DirectMaps())``."""
+        if isinstance(self._entry, list):
+            return list(map(operator.itemgetter(key), self._entry))
+        else:
+            return self._entry[key]
+
+
 class DirectMaps(object):
     """An abstraction around the remote matches.
 
@@ -317,9 +346,9 @@ class DirectMaps(object):
         """Used by Python when executing ``''.format(*DirectMaps())``."""
         value = self._matches[idx]
         if isinstance(value, list) and len(value) == 1:
-            return value[0]
+            return DirectMap(value[0])
         else:
-            return value
+            return DirectMap(value)
 
 
 def validate_mapping_structure(ref):
