5d7bcd09a8e4ccb937188b375aa5004bf6a5541e
Michael Sherman <shermanm@uchicago.edu>
2024-01-19 19:22:59 +0000

    fix handling of empty "extras" case during mapping
    Our modification to handle "extra" project properties during mapping
    caused the "extra" parameter to be added in all cases, even when empty,
    breaking existing unit-tests.
    
    This is a simple refactor to avoid adding the property when it is
    otherwise unused, preserving the upstream unit-tests, as well as a
    new unit-test to specifically exercise the "extra" feature.
    
    (cherry picked from commit 796f0fa282cf576ba695aa0202d1204ff5b48b6b)

--- keystone/tests/unit/mapping_fixtures.py
+++ keystone/tests/unit/mapping_fixtures.py
@@ -1603,6 +1603,16 @@ EMPLOYEE_ASSERTION_MULTIPLE_GROUP_TYPES = {
     'orgPersonBuilding': 'BuildingX;BuildingY'
 }
 
+EMPLOYEE_ASSERTION_MULTIPLE_GROUP_TYPES_EXTRA = {
+    'Email': 'tim@example.com',
+    'UserName': 'tbo',
+    'FirstName': 'Tim',
+    'LastName': 'Bo',
+    'orgPersonType': 'Developer;Employee',
+    'orgPersonBuilding': 'BuildingX;BuildingY',
+    'orgPersonBuildingNickName': 'BuildingXNick;'
+}
+
 EMPLOYEE_ASSERTION_PREFIXED = {
     'PREFIX_Email': 'tim@example.com',
     'PREFIX_UserName': 'tbo',
@@ -1972,6 +1982,38 @@ MAPPING_PROJECT_MAPPED_NAME_AND_ROLES = {
                 },
                 {
                     "type": "orgPersonType"
+                }
+            ]
+        }
+    ]
+}
+
+MAPPING_PROJECTS_MAPPED_NAME_AND_ROLES_AND_EXTRA = {
+    "rules": [
+        {
+            "local": [
+                {
+                    "user": {
+                        "name": "{0}",
+                    }
+                },
+                {
+                    "projects": [
+                        {"name": "{1}",
+                         "extra": {"nickname": "{2}"},
+                         "roles": [{"name": "member"}]}
+                    ]
+                }
+            ],
+            "remote": [
+                {
+                    "type": "UserName"
+                },
+                {
+                    "type": "orgPersonBuilding"
+                },
+                {
+                    "type": "orgPersonBuildingNickName"
                 }
             ]
         }
