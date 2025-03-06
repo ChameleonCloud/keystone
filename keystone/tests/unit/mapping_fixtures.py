c0539bc8f386b3695496221159c3899e0d404657
Michael Sherman <shermanm@uchicago.edu>
2025-03-06 20:53:00 +0000

    Direct map auto-provision project names and roles
    This adds support for direct-mapping auto-provisioned projects and
    roles, similarly to how the existing groups functionality works.
    
    Note: if mapping role names, the roles must already exist in the
    federated domain for the mapping to succeed, as roles are not
    automatically provisioned in the same way that projects are.
    
    Change-Id: I390a26ab3d87480cb82c603e3b8712a9c8132e1b
    Closes-Bug: #1878496
    
    Conflicts:
    	keystone/federation/utils.py
    	keystone/tests/unit/contrib/federation/test_utils.py
    These conflicts are from an additional change to the groups
    parsing does not yet exist on the Train branch.

--- keystone/tests/unit/mapping_fixtures.py
+++ keystone/tests/unit/mapping_fixtures.py
@@ -1594,6 +1594,15 @@ EMPLOYEE_ASSERTION_MULTIPLE_GROUPS = {
     'Thing': 'yes!;maybe!;no!!'
 }
 
+EMPLOYEE_ASSERTION_MULTIPLE_GROUP_TYPES = {
+    'Email': 'tim@example.com',
+    'UserName': 'tbo',
+    'FirstName': 'Tim',
+    'LastName': 'Bo',
+    'orgPersonType': 'Developer;Employee',
+    'orgPersonBuilding': 'BuildingX;BuildingY'
+}
+
 EMPLOYEE_ASSERTION_PREFIXED = {
     'PREFIX_Email': 'tim@example.com',
     'PREFIX_UserName': 'tbo',
@@ -1802,15 +1811,6 @@ MAPPING_PROJECTS = {
             "remote": [
                 {
                     "type": "UserName"
-                },
-                {
-                    "type": "Email",
-                },
-                {
-                    "type": "orgPersonType",
-                    "any_one_of": [
-                        "Employee"
-                    ]
                 }
             ]
         }
@@ -1864,5 +1864,110 @@ MAPPING_PROJECTS_WITHOUT_NAME = {
                 }
             ]
         },
+    ]
+}
+
+MAPPING_PROJECTS_MAPPED_NAME = {
+    "rules": [
+        {
+            "local": [
+                {
+                    "user": {
+                        "name": "{0}"
+                    }
+                },
+                {
+                    "projects": [
+                        {
+                            "name": "{1}",
+                            "roles": [
+                                {"name": "member"}
+                            ]
+                        }
+                    ]
+                }
+            ],
+            "remote": [
+                {
+                    "type": "UserName"
+                },
+                {
+                    "type": "orgPersonBuilding"
+                }
+            ]
+        }
+    ]
+}
+
+MAPPING_PROJECTS_MAPPED_ROLES = {
+    "rules": [
+        {
+            "local": [
+                {
+                    "user": {
+                        "name": "{0}"
+                    }
+                },
+                {
+                    "projects": [
+                        {
+                            "name": "Production",
+                            "roles": [
+                                {"name": "{1}"}
+                            ]
+                        },
+                        {
+                            "name": "Project for {0}",
+                            "roles": [
+                                {"name": "Admin"}
+                            ]
+                        }
+                    ]
+                }
+            ],
+            "remote": [
+                {
+                    "type": "UserName"
+                },
+                {
+                    "type": "orgPersonType"
+                }
+            ]
+        }
+    ]
+}
+
+MAPPING_PROJECT_MAPPED_NAME_AND_ROLES = {
+    "rules": [
+        {
+            "local": [
+                {
+                    "user": {
+                        "name": "{0}"
+                    }
+                },
+                {
+                    "projects": [
+                        {
+                            "name": "{1}",
+                            "roles": [
+                                {"name": "{2}"}
+                            ]
+                        }
+                    ]
+                }
+            ],
+            "remote": [
+                {
+                    "type": "UserName"
+                },
+                {
+                    "type": "orgPersonBuilding"
+                },
+                {
+                    "type": "orgPersonType"
+                }
+            ]
+        }
     ]
 }
