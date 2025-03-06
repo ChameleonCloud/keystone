940b4f724192ebd9ed96b2f38d629274fb6e1722
Michael Sherman <shermanm@uchicago.edu>
2025-03-06 20:53:00 +0000

    Direct map auto-provision project names and roles
    This adds support for direct-mapping auto-provisioned projects and
    roles, similarly to how the existing groups functionality works.
    
    Note: if mapping role names, the roles must already exist in the
    federated domain for the mapping to succeed, as roles are not
    automatically provisioned in the same way that projects are.
    
    Change-Id: I390a26ab3d87480cb82c603e3b8712a9c8132e1b
    Closes-Bug: #1878496
    
    Conflicts:
    	keystone/federation/utils.py
    	keystone/tests/unit/contrib/federation/test_utils.py
    These conflicts are from an additional change to the groups
    parsing does not yet exist on the Train branch.

--- keystone/federation/utils.py
+++ keystone/federation/utils.py
@@ -597,6 +597,34 @@ class RuleProcessor(object):
                            group_names_list]
         return group_dicts
 
+    def _normalize_projects(self, identity_value):
+        project_dicts = []
+        for project in identity_value['projects']:
+            project_names_list = self._expand_listlike(project['name'])
+            role_names_list = []
+            for role in project['roles']:
+                role_names_list.extend(self._expand_listlike(role['name']))
+            project_dicts.extend([{'name': p_name, 'roles': [
+                                    {'name': r_name}
+                                    for r_name in role_names_list
+                                 ]} for p_name in project_names_list])
+        return project_dicts
+
+    def _expand_listlike(self, value):
+        """If value is a string representation of a list, parse it to a real
+        list. Also, if the provided value contains only one element, it will
+        be parsed as a simple string, and not a list or the representation
+        of a list.
+
+        This is necessary due to the way we do direct mapping substitutions
+        today (see function _update_local_mapping())
+        """
+        try:
+            value_list = ast.literal_eval(value)
+        except (ValueError, SyntaxError):
+            value_list = [value]
+        return value_list
+
     def _transform(self, identity_values):
         """Transform local mappings, to an easier to understand format.
 
@@ -698,7 +726,8 @@ class RuleProcessor(object):
                 except (ValueError, SyntaxError):
                     group_ids.update([identity_value['group_ids']])
             if 'projects' in identity_value:
-                projects = identity_value['projects']
+                project_dicts = self._normalize_projects(identity_value)
+                projects.extend(project_dicts)
 
         normalize_user(user)
 
